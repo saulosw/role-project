39d0f8aa2c6491c7ec29a55b20cb89b9f727a11c
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2025-10-05 13:26:01 -0300

    feat: about us page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Homepage from './pages/HomePage/Homepage';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import AboutUs from './pages/AboutUs/AboutUs';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/sobre" element={<AboutUs />} />
       </Routes>
     </Router>
   );
